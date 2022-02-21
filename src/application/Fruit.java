8a23bccd1e1d2a6921dddd566a266e30e2117aad
earl-stephens <stephensearl@hotmail.com>
2022-02-20 23:47:30 -0700

    Setup an enum constructor

--- src/application/Fruit.java
+++ src/application/Fruit.java
@@ -1,5 +1,17 @@
 package application;
 
 public enum Fruit {
-	APPLE, BANANA, ORANGE;
+	APPLE("green"), BANANA("yellow"), ORANGE("orange");
+	//Constants have to be the first line in the enum
+	//These constants are actually instances of the enum
+	
+	//constructor can't be public
+	private String color;
+	Fruit(String color) {
+		this.color = color;
+	}
+	
+	public String toString() {
+		return super.toString().toLowerCase() + " (" + color + ")";
+	}
 }
